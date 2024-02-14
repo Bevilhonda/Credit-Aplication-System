c0ec73fd80028179b7d95af70ee4777732dbc30b
Bevilhonda <marcelinyuri@gmail.com>
2024-02-14 20:58:38 -0300

    cria função post

--- src/main/kotlin/Curso/Kotlin/credit/aplication/system/controllers/CreditResource.kt
+++ src/main/kotlin/Curso/Kotlin/credit/aplication/system/controllers/CreditResource.kt
@@ -1,4 +1,19 @@
 package Curso.Kotlin.credit.aplication.system.controllers
 
-class CreditResource {
+import Curso.Kotlin.credit.aplication.system.Service.Implement.CreditService
+import Curso.Kotlin.credit.aplication.system.controllers.dto.CreditDto
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/credits")
+class CreditResource(
+    private val creditservice : CreditService
+) {
+
+    @PostMapping("/insert")
+    fun saveCredit(creditDto : CreditDto): String{
+
+    }
 }
