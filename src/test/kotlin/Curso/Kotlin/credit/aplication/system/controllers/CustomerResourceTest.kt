a95f726c65d998b5916267e2113410b5fa5d9485
Bevilhonda <marcelinyuri@gmail.com>
2024-02-16 10:15:28 -0300

    cria classes de testes

---
+++ src/test/kotlin/Curso/Kotlin/credit/aplication/system/controllers/CustomerResourceTest.kt
@@ -0,0 +1,57 @@
+package Curso.Kotlin.credit.aplication.system.controllers
+
+import Curso.Kotlin.credit.aplication.system.Repository.CustomerRepository
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@ContextConfiguration
+class CustomerResourceTest {
+    @Autowired
+    private lateinit var customerRepository: CustomerRepository
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    companion object {
+        const val URL: String = "/api/customers"
+    }
+
+    @BeforeEach
+    fun setup() = customerRepository.deleteAll()
+
+    @AfterEach
+    fun tearDown() = customerRepository.deleteAll()
+
+
+    @Test
+    fun saveCustomer() {
+    }
+
+    @Test
+    fun findById() {
+    }
+
+    @Test
+    fun deleteCustomer() {
+    }
+
+    @Test
+    fun updateCostumer() {
+    }
+}
